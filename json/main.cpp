5ce3a9468c7fc26f08efb42fb93b3315b6650f69
devinjh <44875825+devinjh@users.noreply.github.com>
2019-03-27 10:34:16 -0700

    Initial commit
    Made some minor changes to Dr. Sutton's JSON parser. Also, if downloaded, be sure to put the cpp.json into the build directory. Otherwise, the program will not find the file.

---
+++ json/main.cpp
@@ -0,0 +1,33 @@
+
+#include "json.hpp"
+
+#include <iterator>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[])
+{
+  std::ifstream infile;
+  infile.open("cpp.json");
+  if (infile)
+  {
+    std::string s2 = "";
+    getline(infile, s2);
+    std::cout << s2 << std::endl;
+
+    std::cout << "\n---\n" << std::endl;
+
+    std::istringstream inn(s2);
+    std::istreambuf_iterator<char> first(inn);
+    std::istreambuf_iterator<char> last;
+    std::string s3(first, last);
+    json::Value* val = json::parse(s3);
+    std::cout << *val << '\n';
+  }
+  else
+  {
+    std::cout << "did not open file" << std::endl;
+  }
+}
