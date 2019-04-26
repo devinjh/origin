c2075dfc4839372175b8536a0dfb3e837f855426
devinjh <44875825+devinjh@users.noreply.github.com>
2019-04-26 13:12:27 -0700

    Initial commit

---
+++ racing/gameEvents.cpp
@@ -0,0 +1,5 @@
+// This is the gameEvents.cpp file for the race game
+// Devin Hopkins
+// 4190350
+
+#include "gameEvents.hpp"
---
+++ racing/gameEvents.hpp
@@ -0,0 +1,11 @@
+// This is the game.hpp file for the race game
+// Devin Hopkins
+// 4190350
+
+#ifndef GAMEEVENTS_HPP
+#define GAMEEVENTS_HPP
+
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+#endif
